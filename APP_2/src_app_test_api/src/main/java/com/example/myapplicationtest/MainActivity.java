fba6a173500c28ca4b6e39b505a7e72cdb84bc7b
Lawaj <pierrick.dumas@isen.yncrea.fr>
2021-02-11 11:02:06 +0100

    PushProject

---
+++ APP_2/src_app_test_api/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapplicationtest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewResult = findViewById(R.id.textviewresult);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://6007f1a4309f8b0017ee5022.mockapi.io/api/m1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post : posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "Nom de compte: " + post.getAccount_name() + "\n";
+                    content += "Somme: " + post.getAmount() + "\n";
+                    content += "IBAN " + post.getIban() + "\n";
+                    Log.d("contenu",content);
+                    textViewResult.append(content);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
---
+++ APP_2/src_main_app/src/main/java/com/example/myapptest/Post.java
@@ -0,0 +1,22 @@
+package com.example.myapptest;
+import java.util.List;
+public class Post {
+    
+    private String id;
+    private String iban;
+    private String account_name;
+    private String amount;
+
+    public String getId() {
+        return id;
+    }
+    public String getAccount_name() {
+        return account_name;
+    }
+    public String getAmount() {
+        return amount;
+    }
+    public String getIban() {
+        return iban;
+    }
+}
